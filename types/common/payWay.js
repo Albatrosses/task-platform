de84152a3f8031eb7d7fe87b7d70c5dd76a84460
Albatrosses <capitalism_4418@163.com>
2020-01-10 19:36:47 +0800

    Albatrosses: Add

--- types/common/payWay.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var PAY_WAY_CODE;
-(function (PAY_WAY_CODE) {
-    PAY_WAY_CODE[PAY_WAY_CODE["WECHAT"] = 1] = "WECHAT";
-    PAY_WAY_CODE[PAY_WAY_CODE["ALIPAY"] = 2] = "ALIPAY";
-})(PAY_WAY_CODE = exports.PAY_WAY_CODE || (exports.PAY_WAY_CODE = {}));
